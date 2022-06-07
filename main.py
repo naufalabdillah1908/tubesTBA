0b462c8a2950e901b7c55a81422fb6f5d2bbb6e9
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2022-06-07 17:51:03 +0700

    fix3

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def LA(sentence):
     if state == 'q35':
       st.write(current, ' --> valid')
     if state == 'error':
-      st.write('error in: ', current, "with the id of ", idx, 'and state of', f"'{before_estate}'")
+      st.write('error in: ', current, "with the id of ", idx, 'and state of', before_state)
       break;
     idx = idx + 1
 
